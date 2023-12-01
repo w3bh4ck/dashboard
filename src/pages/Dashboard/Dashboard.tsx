c251ac49b0818ec9771ec2d267e4e33c7c360fba
w3bh4ck <amadiluckysampson@gmail.com>
2023-12-01 06:40:49 +0100

    Feat_: Add report actions

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import PageWrapper from "../../containers/PageWrapper/PageWrapper";
 import DashboardCard from "../../components/Cards/DashboardCard";
+import { Icon } from "../../components/Common/Icons";
 
 const Dashboard = () => {
   return (
@@ -32,6 +33,34 @@ const Dashboard = () => {
             amount="1,652.50"
           />
         </div>
+
+        {/* Dashboard report */}
+        <StyledDashboardReport>
+          <div className="action-section">
+            <div className="title">Sales</div>
+            <div className="days active">7 days</div>
+            <div className="days">30 days</div>
+            <div>
+              <StyledMenuButton>
+                <span>USD</span>
+                <span>
+                  <Icon className="" svg="down-arrow" />
+                </span>
+              </StyledMenuButton>
+            </div>
+            <div>
+              <StyledInput>
+                <input placeholder="Email" />
+              </StyledInput>
+            </div>
+            <div>
+              <StyledButton>
+                <Icon svg="download-arrow" />
+                Download Report
+              </StyledButton>
+            </div>
+          </div>
+        </StyledDashboardReport>
       </StyledMain>
     </PageWrapper>
   );
@@ -81,4 +110,64 @@ const StyledMain = styled.main`
     font-weight: 600;
     padding: 10px 0px 0px 20px;
   }
+`;
+
+const StyledDashboardReport = styled.div`
+  .action-section {
+    display: flex;
+    margin-top: 40px;
+    font-size: 14px;
+    & .title {
+      font-size: 20px;
+      margin: 8px 10px;
+    }
+    & .days {
+      font-size: 14px;
+      font-weight: 600;
+      margin: 10px;
+    }
+    & .active {
+      color: red;
+    }
+  }
+`;
+
+const StyledMenuButton = styled.div`
+  display: flex;
+  cursor: pointer;
+  justify-content: center;
+  border-radius: 6px;
+  border: 1px solid #0a0a0a;
+  height: 40px;
+  width: 91px;
+  & span {
+    margin-top: 10px;
+  }
+`;
+
+const StyledInput = styled.div`
+  width: 283px;
+  position: relative;
+  padding-left: 10px;
+  border-radius: 8px;
+  border: 1px solid #f4f4f4;
+  margin-left: 10px;
+  margin-right: 10px;
+  & input {
+    width: 80%;
+    border: none;
+    height: 40px;
+    outline: none;
+  }
+`;
+
+const StyledButton = styled.button`
+  height: 40px;
+  width: 148px;
+  border-radius: 8px;
+  border: 1px solid #0a0a0a;
+  background-color: #fff;
+  display: flex;
+  justify: center;
+  padding-top: 11px;
 `;
